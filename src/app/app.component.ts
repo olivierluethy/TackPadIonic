c226e01673ec69219c24215b1b1eb25a4dd98b7f
Oli7000 <olivier.luethy@gmx.net>
2022-01-25 13:41:42 +0100

    Adding plus button on top of navigaton bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Profile', url: '/profile', icon: 'contacts' },
-    { title: 'Notizen', url: '/notizen', icon: 'notes' }
+    { title: 'Profile', url: '/profile', icon: 'person' },
+    { title: 'Notizen', url: '/notizen', icon: 'copy' }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
